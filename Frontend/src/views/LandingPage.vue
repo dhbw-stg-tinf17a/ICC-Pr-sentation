3b099739c60bbb1738af8df625ec878868ed420e
Felix Schütz <mail@felix-schuetz.de>
2020-03-02 21:47:44 +0100

    Test webhook deployment

--- Frontend/src/views/LandingPage.vue
+++ Frontend/src/views/LandingPage.vue
@@ -1,24 +1,13 @@
 <template>
   <div class="hero-body">
-    <div
-      id="container"
-      class="container has-text-centered"
-    >
+    <div id="container" class="container has-text-centered">
       <div class="column is-6 is-offset-3">
-        <h1 class="title is-1">
-          Welcome to Gunter!
-        </h1>
-        <h2 class="subtitle is-4">
-          Worlds smartest PDA
-        </h2>
+        <h1 class="title is-1">Welcome to the one and only Gunter!</h1>
+        <h2 class="subtitle is-4">Worlds smartest PDA</h2>
         <div class="box">
           <div class="field is-grouped">
             <p class="control is-expanded">
-              <input
-                class="input"
-                type="text"
-                placeholder="Start speaking or typing"
-              >
+              <input class="input" type="text" placeholder="Start speaking or typing" />
             </p>
           </div>
         </div>
@@ -29,6 +18,6 @@
 
 <style scoped>
 #container {
-    margin-top: 12rem;
+  margin-top: 12rem;
 }
 </style>
