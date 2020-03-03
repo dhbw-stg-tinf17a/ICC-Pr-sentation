f0a93f677c99312aefd85452c9889db9e223ad44
Arbër <Arber.Rrapaj@hotmail.de>
2020-03-03 09:43:17 +0100

    add api route to match felix's backend

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -2,13 +2,21 @@ const router = require('express').Router();
 
 const pino = require('pino');
 const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+const welcomeMessage = "Welcome to Gunter's heart - I am the backend. Feel free to leave, since you should let the frontend talk to me.";
 
 /**********************
  **** Landing page ****
  **********************/
 router.get('/', (req, res) => {
 	logger.trace("router - index - GET called on /");
-	const welcomeMessage = "Welcome to Gunter's heart - I am the backend. Feel free to leave, since you should let the frontend talk to me.";
+	res.status(200).send({ status: 200, data: welcomeMessage });
+});
+
+/**********************
+ **** API page for Felix ****
+ **********************/
+router.get('/api', (req, res) => {
+	logger.trace("router - index - GET called on /api");
 	res.status(200).send({ status: 200, data: welcomeMessage });
 });
 
