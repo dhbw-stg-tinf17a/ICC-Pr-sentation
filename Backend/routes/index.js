e0c96a23777b110a0b64275ade664bd8a98166ca
Felix Schütz <mail@felix-schuetz.de>
2020-03-03 10:19:33 +0100

    Serve all routes under /api

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -2,21 +2,11 @@ const router = require('express').Router();
 
 const pino = require('pino');
 const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
-const welcomeMessage = "Welcome to Gunter's heart - I am the backend. Feel free to leave, since you should let the frontend talk to me.";
+const welcomeMessage = 'Welcome to Gunter\'s heart - I am the backend.Feel free to leave, since you should let the frontend talk to me.';
 
-/**********************
- **** Landing page ****
- **********************/
+/** Landing page */
 router.get('/', (req, res) => {
-	logger.trace("router - index - GET called on /");
-	res.status(200).send({ status: 200, data: welcomeMessage });
-});
-
-/**********************
- **** API page for Felix ****
- **********************/
-router.get('/api', (req, res) => {
-	logger.trace("router - index - GET called on /api");
+	logger.trace('router - index - GET called on /');
 	res.status(200).send({ status: 200, data: welcomeMessage });
 });
 
