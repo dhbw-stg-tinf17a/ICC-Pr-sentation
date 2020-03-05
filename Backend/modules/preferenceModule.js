94d85d5067b14e2827d82098f3e78e1b647102da
Arbër <Arber.Rrapaj@hotmail.de>
2020-03-05 09:26:45 +0100

    add preferenceModule-database template

--- Backend/modules/preferenceModule.js
+++ Backend/modules/preferenceModule.js
@@ -1,20 +1,8 @@
-const pino = require('pino');
-const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+// const logger = require('pino')({ level: process.env.LOG_LEVEL || 'info' });
 
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('preferenceModule.json');
 const db = low(adapter);
-
-const defaultUser = {
-	"name": "Gunter",
-	"age": 107,
-	"favouriteFood": "Jelly Beans",
-	"loyal": true
-};
-
-// Set some defaults
-db.defaults({ user: defaultUser, global: {},  })
-	.write();
 
 module.exports = db;
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -8,8 +8,7 @@ const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
  **********************/
 router.get('/', (req, res) => {
 	logger.trace("router - index - GET called on /");
-	const welcomeMessage = "Welcome to Gunter's heart - I am the backend. Feel free to leave, since you should let the frontend talk to me.";
-	res.status(200).send({ status: 200, data: welcomeMessage });
+	const welcomeMessage = 'Welcome to Gunter\'s heart - I am the backend.Feel free to leave, since you should let the frontend talk to me.';	res.status(200).send({ status: 200, data: welcomeMessage });
 });
 
 router.use('/user', require('./user'));
