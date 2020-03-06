b777d55bef5695eafd76f79d30dd98980a8abaf6
Felix Schütz <mail@felix-schuetz.de>
2020-03-06 14:43:09 +0100

    Ask for notification permission, subscribe to push notifications

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -14,10 +14,34 @@ new Vue({
 }).$mount('#app');
 
 window.addEventListener('load', async () => {
+  console.log(process.env.PUSH_KEY_PUBLIC);
+
   try {
-    const registration = await navigator.serviceWorker.register('/service-worker.js');
-    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    const serviceWorkerRegistration = await navigator.serviceWorker.register('/service-worker.js');
+    console.log('Service worker ✔');
+
+    // not necessarily required, implied by serviceWorkerRegistration.pushManager.subscribe
+    const notificationPermission = await Notification.requestPermission();
+    if (notificationPermission === 'granted') {
+      console.log('Notification permission ✔');
+    } else {
+      console.log('Notification permission ✘');
+      return;
+    }
+
+    const pushSubscription = await serviceWorkerRegistration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: 'BBauGh8G3IdDf28vFQD0-Nn-8wniZUsCjvRa0F0MbRUTmy0NDDGQCT-OD3M5k8c54DNsyw9-_SwibbBXxWYG_nk',
+    });
+    console.log('Push subscription ✔');
+
+    await fetch('/api/push/subscribe', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(pushSubscription),
+    });
+    console.log('Push subscription sent to server ✔');
   } catch (err) {
-    console.log('ServiceWorker registration failed: ', err);
+    console.log('Something went wrong: ', err);
   }
 });
