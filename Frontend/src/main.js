76424b85892c1588db959fb5f73f5cd704775a61
Felix Schütz <mail@felix-schuetz.de>
2020-03-09 15:39:14 +0100

    Only enable notifications if the user decides to do so

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -5,13 +5,12 @@ import persistentState from 'vue-persistent-state';
 import App from './App.vue';
 import router from './router';
 
-
 Vue.config.productionTip = false;
 
 Vue.use(Buefy);
 
 Vue.use(persistentState, {
-  sendNotifications: true,
+  notificationsEnabled: true,
 });
 
 new Vue({
@@ -20,35 +19,5 @@ new Vue({
 }).$mount('#app');
 
 window.addEventListener('load', async () => {
-  try {
-    const serviceWorkerRegistration = await navigator.serviceWorker.register('/service-worker.js');
-    console.log('✔ Service worker');
-
-    // not necessarily required, implied by serviceWorkerRegistration.pushManager.subscribe
-    const notificationPermission = await Notification.requestPermission();
-    if (notificationPermission !== 'granted') {
-      console.log('✘ Notification permission');
-      return;
-    }
-    console.log('✔ Notification permission');
-
-    const pushSubscription = await serviceWorkerRegistration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: 'BBauGh8G3IdDf28vFQD0-Nn-8wniZUsCjvRa0F0MbRUTmy0NDDGQCT-OD3M5k8c54DNsyw9-_SwibbBXxWYG_nk',
-    });
-    console.log('✔ Push subscription');
-
-    const response = await fetch('/api/push/subscribe', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(pushSubscription),
-    });
-    if (!response.ok) {
-      console.log('✘ Push subscribption could not be sent to server');
-      return;
-    }
-    console.log('✔ Push subscription sent to server');
-  } catch (err) {
-    console.log('Something went wrong: ', err);
-  }
+  await navigator.serviceWorker.register('/service-worker.js');
 });
--- Frontend/src/views/Preferences.vue
+++ Frontend/src/views/Preferences.vue
@@ -1,15 +1,72 @@
 <template>
   <div class="container">
     <div class="section">
-    <h1 class="title">
-      Preferences
-    </h1>
+      <h1 class="title">
+        Preferences
+      </h1>
 
       <div class="field">
-        <b-checkbox v-model="sendNotifications">
+        <b-checkbox v-model="notificationsEnabled">
           Send notifications
         </b-checkbox>
-  </div>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  watch: {
+    async notificationsEnabled() {
+      if (this.notificationsEnabled) {
+        await this.enableNotifications();
+      } else {
+        await this.disableNotifications();
+      }
+    },
+  },
+  methods: {
+    async enableNotifications() {
+      try {
+        const notificationPermission = await Notification.requestPermission();
+        if (notificationPermission !== 'granted') {
+          this.$buefy.snackbar.open({
+            message: 'Unfortunately, Gunter doesn\'t have permission to send you notifications. Please check your browser settings.',
+            type: 'is-danger',
+          });
+          this.notificationsEnabled = false;
+          return;
+        }
+
+        const serviceWorkerRegistration = await navigator.serviceWorker.ready;
+        const pushSubscription = await serviceWorkerRegistration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: 'BBauGh8G3IdDf28vFQD0-Nn-8wniZUsCjvRa0F0MbRUTmy0NDDGQCT-OD3M5k8c54DNsyw9-_SwibbBXxWYG_nk',
+        });
+
+        const response = await fetch('/api/notifications/enable', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(pushSubscription),
+        });
+        if (!response.ok) {
+          this.$buefy.snackbar.open({
+            message: 'Unfortunately, Gunter couldn\'t enable notifications for you. He doesn\'t know why and is truly sorry.',
+            type: 'is-danger',
+          });
+          this.notificationsEnabled = false;
+        }
+      } catch (err) {
+        this.$buefy.snackbar.open({
+          message: 'Unfortunately, Gunter couldn\'t enable notifications for you. He doesn\'t know why and is truly sorry.',
+          type: 'is-danger',
+        });
+        this.notificationsEnabled = false;
+      }
+    },
+    async disableNotifications() {
+      // TOOD
+    },
+  },
+};
+</script>
