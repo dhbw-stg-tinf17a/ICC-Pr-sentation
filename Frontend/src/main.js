3c9e03cc00da61689418d754f5d8a7334728de4f
Felix Schütz <mail@felix-schuetz.de>
2020-03-09 15:03:57 +0100

    Use vue-persistent-state

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,12 +1,18 @@
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 import Vue from 'vue';
+import persistentState from 'vue-persistent-state';
 import App from './App.vue';
 import router from './router';
+
 
 Vue.config.productionTip = false;
 
 Vue.use(Buefy);
+
+Vue.use(persistentState, {
+  sendNotifications: true,
+});
 
 new Vue({
   router,
