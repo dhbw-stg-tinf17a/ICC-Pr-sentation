c85300967f68d9d1685f86d9c7079c253584d409
Felix Schütz <mail@felix-schuetz.de>
2020-03-09 13:42:43 +0100

    Correct linter errors

--- Frontend/public/service-worker.js
+++ Frontend/public/service-worker.js
@@ -1,11 +1,13 @@
-self.addEventListener('push', async event => {
+/* eslint no-restricted-globals: 1 */
+
+self.addEventListener('push', async (event) => {
   event.waitUntil(self.registration.showNotification('Gunter says hello', {
-    body: 'This is only a test',
+    body: event.data.text(),
     icon: '/favicon.jpg',
-    badge: '/bade.png'
+    badge: '/bade.png',
   }));
 });
 
-self.addEventListener('notificationclick', function (event) {
+self.addEventListener('notificationclick', (event) => {
   event.notification.close();
 });
