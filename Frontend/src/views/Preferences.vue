94f8d9c5fff4cf0935d7c74085dc2ac8b76b077a
Felix Schütz <mail@felix-schuetz.de>
2020-03-09 15:21:33 +0100

    Add checkbox to control notifications

--- Frontend/src/views/Preferences.vue
+++ Frontend/src/views/Preferences.vue
@@ -1,10 +1,15 @@
 <template>
-  <div
-    id="preferences"
-    class="hero-body"
-  >
+  <div class="container">
+    <div class="section">
     <h1 class="title">
       Preferences
     </h1>
+
+      <div class="field">
+        <b-checkbox v-model="sendNotifications">
+          Send notifications
+        </b-checkbox>
+  </div>
+    </div>
   </div>
 </template>
