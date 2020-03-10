8c916265b88c574bae2715a46430b6ed391ac52c
Wetzel <manuel.wetzel@hpe.com>
2020-03-10 15:50:04 +0100

    set header for put and use querystring

--- Frontend/src/services/Location.js
+++ Frontend/src/services/Location.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
+import qs from 'querystring';
 
 const api = axios.create({
   baseURL: '/api/',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
 });
 
 const functions = {
   sendPosition(location) {
-    return api.put('user/coordinates', location);
+    return api.put('user/coordinates', qs.stringify(location));
   },
 };
 
