ae126442e65dd9176ca4444a617e3339debdce8d
Wetzel <manuel.wetzel@hpe.com>
2020-03-10 10:06:48 +0100

    Improved 404 page

--- Frontend/src/views/Default404.vue
+++ Frontend/src/views/Default404.vue
@@ -3,12 +3,18 @@
     id="Default404"
     class="hero-body"
   >
-    <h1>Uuuuuups. Diese Seite existiert nicht.</h1>
-    <router-link
-      class="router-link"
-      :to="{name: 'landingPage'}"
-    >
-      <h2>Zurück zur Startseite</h2>
-    </router-link>
+    <div class="container">
+      <h1 class="is-size-3">
+        Sorry. This page doesnt exist.
+      </h1>
+      <router-link
+        class="router-link"
+        :to="{name: 'landingPage'}"
+      >
+        <div class="button">
+          Back to Home
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
