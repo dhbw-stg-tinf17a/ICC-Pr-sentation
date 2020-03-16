ea06b5c10d317608e6d227f821b2d17c96915c3b
Wetzel <manuel.wetzel@hpe.com>
2020-03-16 20:50:16 +0100

    extended App.vue test to cover mixin

--- Frontend/tests/unit/App.spec.js
+++ Frontend/tests/unit/App.spec.js
@@ -2,6 +2,7 @@ import { createLocalVue, mount } from '@vue/test-utils';
 import Buefy from 'buefy';
 import VueRouter from 'vue-router';
 import mockAxios from 'axios';
+import SpeechService from '@/services/SpeechSynthesis';
 import App from '@/App.vue';
 
 const localVue = createLocalVue();
@@ -14,11 +15,32 @@ const routes = [
     name: 'landingPage',
     component: App,
   },
+  {
+    path: '/trainer',
+    name: 'trainer',
+    component: App,
+  },
+  {
+    path: '/restaurant',
+    name: 'restaurant',
+    component: App,
+  },
+  {
+    path: '/commute',
+    name: 'commute',
+    component: App,
+  },
+  {
+    path: '/travel',
+    name: 'travel',
+    component: App,
+  },
 ];
 const router = new VueRouter({
   routes,
 });
 
+jest.mock('@/services/SpeechSynthesis');
 jest.mock('axios', () => ({
   put: jest.fn(() => Promise.reject()),
   create: () => mockAxios,
@@ -62,6 +84,34 @@ describe('App.vue', () => {
     wrapper.vm.error('ERROR');
     wrapper.vm.$nextTick(() => {
       expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('test SpeechRecognitionLogic.js mixin', () => {
+    it('route change, when userInput equals keyword', () => {
+      const keywords = ['trainer', 'commute', 'restaurant', 'travel'];
+      const wrapper = factory();
+      for (let i = 0; i < keywords.length; i += 1) {
+        wrapper.vm.checkForUseCase(keywords[i]);
+        expect(router.currentRoute.name).toBe(keywords[i]);
+      }
+      wrapper.vm.checkForUseCase('home');
+      expect(router.currentRoute.name).toBe('landingPage');
+    });
+
+    it('time is read, when user inputs time keyword', () => {
+      const wrapper = factory();
+      wrapper.vm.checkForUseCase('time');
+      expect(SpeechService.speak).toHaveBeenCalled();
+    });
+
+    it('userInput watcher calls function', () => {
+      const wrapper = factory();
+      const spy = jest.spyOn(wrapper.vm, 'checkForUseCase');
+      wrapper.vm.userInput = 'Test';
+      wrapper.vm.$nextTick(() => {
+        expect(spy).toHaveBeenCalled();
+      });
     });
   });
 });
