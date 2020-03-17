0efdfb76504ca0849156a8fb5d9bd689eaa4c2a0
Wetzel <manuel.wetzel@hpe.com>
2020-03-17 11:07:32 +0100

    Added SpeechSynthesis to be ignored from test coverage and explained why files are ignored

--- Frontend/jest.config.js
+++ Frontend/jest.config.js
@@ -2,6 +2,13 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   collectCoverage: true,
   collectCoverageFrom: ['**/*.{js,vue}', '!**/node_modules/**', '!**.config.js',
-    '!coverage/**', '!dist/**', '!src/main.js', '!src/router/index.js', '!public/service-worker.js'],
+    '!coverage/**', '!dist/**',
+    '!src/main.js', // ignored because test makes no sense and nothing to test
+    '!src/router/index.js', // not directly testable; tested indirectly in other tests
+    '!public/service-worker.js', // not testable with unit tests; tested manually
+    // SpeechSynthesisUtterance not defined in test run.
+    // Mocking not useful. HTML speech api is already tested from creators, no need to test again.
+    '!/src/services/SpeechSynthesis.js',
+  ],
   coverageReporters: ['html', 'text'],
 };
