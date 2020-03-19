81331571677fc1fb81924685468dd02a868db13b
Felix Schütz <mail@felix-schuetz.de>
2020-03-19 15:12:40 +0100

    Parse response returned from DB prices API

--- Backend/modules/db.js
+++ Backend/modules/db.js
@@ -5,7 +5,7 @@ const options = {
   c: 2, // class
   ohneICE: false, // no ICE trains
   tct: 0, // transfer time
-  dur: 1440, // search for routes in the next 60 minutes
+  dur: 24 * 60 * 60, // search for routes in the next 24 hours
   travellers: [{ bc: 0, typ: 'E', alter: 30 }], // one traveller without BahnCard (bc: 0), adult (typ: 'E'), age of 30 (alter: 30)
   sv: true, // prefer fast routes
   device: 'HANDY', // prevent mimimi
@@ -15,8 +15,8 @@ const endpoint = 'https://ps.bahn.de/preissuche/preissuche/psc_service.go';
 
 module.exports = {
   async getConnections({ start, destination, datetime }) {
-    const date = moment(datetime).format('DD.MM.YY');
-    const time = moment(datetime).format('HH:mm');
+    const date = moment(datetime).tz('Europe/Berlin').format('DD.MM.YY');
+    const time = moment(datetime).tz('Europe/Berlin').format('HH:mm');
 
     const params = {
       data: JSON.stringify({
@@ -39,6 +39,49 @@ module.exports = {
       throw new Error(`Error returned by DB prices API: ${response.data.error.t} - ${response.data.error.tsys}`);
     }
 
-    return response.data;
+    const connections = Object.values(response.data.verbindungen).map((connection) => {
+      const legs = connection.trains.map((leg) => ({
+        from: leg.sn,
+        to: leg.dn,
+        departure: new Date(Number(leg.dep.m)),
+        arrival: new Date(Number(leg.arr.m)),
+        trainNumber: leg.tn,
+      }));
+
+      const { departure } = legs[0];
+      const { arrival } = legs[legs.length - 1];
+      const duration = moment.duration(moment(arrival).diff(departure));
+
+      return {
+        legs,
+        departure,
+        arrival,
+        duration: {
+          hours: duration.hours(),
+          minutes: duration.minutes(),
+        },
+      };
+    });
+
+    const notes = {};
+    Object.entries(response.data.peTexte).forEach(([ref, note]) => {
+      notes[ref] = { name: note.name, text: note.hinweis.replace('<br/>', ' ') };
+    });
+
+    Object.values(response.data.angebote).forEach((offer) => offer.sids.forEach((id) => {
+      connections[Number(id)] = {
+        ...connections[Number(id)],
+        reducedPrice: offer.tt === 'SP',
+        price: Number(offer.p.replace(',', '.')),
+        boundToTrain: offer.zb === 'Y',
+      };
+
+      if (offer.pky in notes) {
+        connections[Number(id)].notes = notes[offer.pky];
+      }
+    }));
+
+
+    return connections;
   },
 };
