f5bd8007d42d5b7d54e7dfd4a817d5b3884b75fb
Arbër <Arber.Rrapaj@hotmail.de>
2020-03-19 21:17:45 +0100

    add tests for weather routes

--- Backend/tests/weather.test.js
+++ Backend/tests/weather.test.js
@@ -1,4 +1,6 @@
+const request = require('supertest');
 const Weather = require('../modules/weather');
+const app = require('../app');
 
 describe('WeatherModule', () => {
   it('getCurrentWeatherForCity - should return a weather object or an specific error object', async () => {
@@ -10,6 +12,46 @@ describe('WeatherModule', () => {
       expect(weather.cod).toEqual(200);
     } catch (error) {
       expect(error).toEqual(new Error('Error with the OpenWeather-API'));
+    }
+  });
+
+  it('route - /weather - should return a weather object or an specific error object', async () => {
+    try {
+      const response = await request(app)
+        .get('/api/weather')
+        .send();
+      expect(response.status).toEqual(200);
+      const weather = response.body;
+      expect(weather).toHaveProperty('data');
+      expect(weather).toHaveProperty('status');
+      expect(weather.status).toEqual(200);
+      expect(weather.data).toHaveProperty('weather');
+      expect(weather.data).toHaveProperty('main');
+      expect(weather.data).toHaveProperty('cod');
+      expect(weather.data.cod).toEqual(200);
+    } catch (error) {
+      // Make sure that error is controlled by us
+      expect(error.status).toEqual(500);
+    }
+  });
+
+  it('route - /weather/Stuttgart - should return a weather object or an specific error object', async () => {
+    try {
+      const response = await request(app)
+        .get('/api/weather/Stuttgart')
+        .send();
+      expect(response.status).toEqual(200);
+      const weather = response.body;
+      expect(weather).toHaveProperty('data');
+      expect(weather).toHaveProperty('status');
+      expect(weather.status).toEqual(200);
+      expect(weather.data).toHaveProperty('weather');
+      expect(weather.data).toHaveProperty('main');
+      expect(weather.data).toHaveProperty('cod');
+      expect(weather.data.cod).toEqual(200);
+    } catch (error) {
+      // Make sure that error is controlled by us
+      expect(error.status).toEqual(500);
     }
   });
 });
