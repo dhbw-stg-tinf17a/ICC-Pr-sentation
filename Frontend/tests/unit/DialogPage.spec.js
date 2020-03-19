9429b1c453f0b70d00d908d495d3f65adfeae93b
Wetzel <manuel.wetzel@hpe.com>
2020-03-19 18:01:53 +0100

    Added DialogPage base test

---
+++ Frontend/tests/unit/DialogPage.spec.js
@@ -0,0 +1,27 @@
+import { createLocalVue, mount } from '@vue/test-utils';
+import Buefy from 'buefy';
+import DialogPage from '@/views/DialogPage.vue';
+
+const localVue = createLocalVue();
+localVue.use(Buefy);
+
+jest.mock('vue-quick-chat');
+jest.mock('vue-quick-chat/dist/vue-quick-chat.css', () => jest.fn());
+
+function factory() {
+  return mount(DialogPage, {
+    localVue,
+    stubs: {
+      Chat: true,
+    },
+  });
+}
+
+describe('DialogPage.vue', () => {
+  describe('Elements are rendered correctly', () => {
+    it('Component renders', () => {
+      const wrapper = factory();
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+});
