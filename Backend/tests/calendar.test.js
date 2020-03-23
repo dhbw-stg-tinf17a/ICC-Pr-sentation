ab7c972d93f3630f8852dadf682105ef0cc871db
Felix Schütz <mail@felix-schuetz.de>
2020-03-23 11:30:27 +0100

    Mock functions only once where desirable

--- Backend/tests/calendar.test.js
+++ Backend/tests/calendar.test.js
@@ -8,20 +8,20 @@ jest.mock('../modules/user');
 describe('calendar module', () => {
   describe('getNextFirstEventOfDay', () => {
     it('should throw an error if no calendar URL is set', async () => {
-      user.getUserPreferences.mockResolvedValue({});
+      user.getUserPreferences.mockResolvedValueOnce({});
 
       await expect(calendar.getFirstEventOfDay()).rejects.toThrow('User has not set their calendar url yet.');
     });
 
     it('should return the first event of today if it did not start yet', async () => {
-      user.getUserPreferences.mockResolvedValue({ calendarUrl: 'https://example.com' });
+      user.getUserPreferences.mockResolvedValueOnce({ calendarUrl: 'https://example.com' });
 
       const now = new Date();
       const inOneMinute = new Date(now.getTime());
       inOneMinute.setUTCMinutes(inOneMinute.getUTCMinutes() + 1);
       const inTwoMinutes = new Date(now.getTime());
       inTwoMinutes.setUTCMinutes(inTwoMinutes.getUTCMinutes() + 1);
-      ical.async.fromURL.mockResolvedValue({
+      ical.async.fromURL.mockResolvedValueOnce({
         1: { type: 'VEVENT', start: inTwoMinutes },
         2: { type: 'VEVENT', start: inOneMinute },
       });
@@ -31,14 +31,14 @@ describe('calendar module', () => {
     });
 
     it('should return the first event of tomorrow if the first event of today already started', async () => {
-      user.getUserPreferences.mockResolvedValue({ calendarUrl: 'https://example.com' });
+      user.getUserPreferences.mockResolvedValueOnce({ calendarUrl: 'https://example.com' });
 
       const now = new Date();
       const oneMinuteAgo = new Date(now.getTime());
       oneMinuteAgo.setUTCMinutes(oneMinuteAgo.getUTCMinutes() - 1);
       const inOneDay = new Date(now.getTime());
       inOneDay.setUTCDate(inOneDay.getUTCDate() + 1);
-      ical.async.fromURL.mockResolvedValue({
+      ical.async.fromURL.mockResolvedValueOnce({
         1: { type: 'VEVENT', start: inOneDay },
         2: { type: 'VEVENT', start: oneMinuteAgo },
       });
