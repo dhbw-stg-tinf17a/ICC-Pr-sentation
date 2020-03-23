b82d7f2830c66377115da2762b0f3d5b5f125c85
Felix Schütz <mail@felix-schuetz.de>
2020-03-23 12:45:27 +0100

    Fix calendar tests

--- Backend/tests/calendar.test.js
+++ Backend/tests/calendar.test.js
@@ -8,37 +8,40 @@ jest.mock('../modules/user');
 describe('calendar module', () => {
   describe('getNextFirstEventOfDay', () => {
     it('should throw an error if no calendar URL is set', async () => {
-      user.getUserPreferences.mockResolvedValueOnce({});
+      user.getUserPreferences.mockResolvedValue({});
 
       await expect(calendar.getFirstEventOfDay()).rejects.toThrow('User has not set their calendar url yet.');
     });
 
     it('should return the first event of today if it did not start yet', async () => {
-      user.getUserPreferences.mockResolvedValueOnce({ calendarUrl: 'https://example.com' });
+      const calendarUrl = 'https://example.com';
+      user.getUserPreferences.mockResolvedValue({ calendarUrl });
 
       const now = new Date();
       const inOneMinute = new Date(now.getTime());
       inOneMinute.setUTCMinutes(inOneMinute.getUTCMinutes() + 1);
       const inTwoMinutes = new Date(now.getTime());
       inTwoMinutes.setUTCMinutes(inTwoMinutes.getUTCMinutes() + 1);
-      ical.async.fromURL.mockResolvedValueOnce({
+      ical.async.fromURL.mockResolvedValue({
         1: { type: 'VEVENT', start: inTwoMinutes },
         2: { type: 'VEVENT', start: inOneMinute },
       });
 
       const event = await calendar.getNextFirstEventOfDay();
       expect(event.start).toStrictEqual(inOneMinute);
+      expect(ical.async.fromURL).toHaveBeenLastCalledWith(calendarUrl);
     });
 
     it('should return the first event of tomorrow if the first event of today already started', async () => {
-      user.getUserPreferences.mockResolvedValueOnce({ calendarUrl: 'https://example.com' });
+      const calendarUrl = 'https://example.com';
+      user.getUserPreferences.mockResolvedValue({ calendarUrl });
 
       const now = new Date();
       const oneMinuteAgo = new Date(now.getTime());
       oneMinuteAgo.setUTCMinutes(oneMinuteAgo.getUTCMinutes() - 1);
       const inOneDay = new Date(now.getTime());
       inOneDay.setUTCDate(inOneDay.getUTCDate() + 1);
-      ical.async.fromURL.mockResolvedValueOnce({
+      ical.async.fromURL.mockResolvedValue({
         1: { type: 'VEVENT', start: inOneDay },
         2: { type: 'VEVENT', start: oneMinuteAgo },
       });
