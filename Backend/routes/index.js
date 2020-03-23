e5a97eafc9228f7e22b2202aa369617f0eed754b
Felix Schütz <mail@felix-schuetz.de>
2020-03-23 20:13:01 +0100

    Remove /api/db routes

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -16,11 +16,10 @@ router.get('/', (req, res) => {
 router.use('/user', require('./user'));
 router.use('/quote', require('./quote'));
 router.use('/weather', require('./weather'));
-router.use('/notifications', require('./notifications'));
 router.use('/places', require('./place'));
-router.use('/db', require('./db'));
 router.use('/usecases', require('./usecase'));
 
+router.use('/notifications', require('./notifications'));
 router.use('/travel-planning', require('./travel-planning'));
 
 router.get('*', (req, res) => {
