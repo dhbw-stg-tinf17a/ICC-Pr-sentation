2b7d960f3f2c0bdf42bd933092a699eccf76e528
Felix Schütz <mail@felix-schuetz.de>
2020-03-26 09:29:21 +0100

    Validate preference updates

--- Backend/modules/preferences.js
+++ Backend/modules/preferences.js
@@ -1,15 +1,22 @@
+const joi = require('@hapi/joi');
 const initDatabase = require('../utilities/init-database');
 
 const database = initDatabase('preferences', {});
 
-// TODO validation
-// TODO explanations
+const schema = joi.object({
+  location: joi.object({
+    latitude: joi.number().min(-90).max(90).required(),
+    longitude: joi.number().min(-180).max(180).required(),
+  }),
+  calendarURL: joi.string().uri({ scheme: ['http', 'https'] }),
+});
 
 async function get() {
   return (await database).value();
 }
 
 async function update(values) {
+  joi.assert(values, schema);
   await (await database).assign(values).write();
 }
 
