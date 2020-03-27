aed537f085bc32309e8c718aae4b51211cef38ec
Felix Schütz <mail@felix-schuetz.de>
2020-03-27 18:31:40 +0100

    Remove logging

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const helmet = require('helmet');
-const pino = require('pino');
 
 const app = express();
-const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
-const prod = process.env.PROD === 'true';
-logger.info(`Running in ${prod ? 'production' : 'development'}`);
 
 app.use(helmet());
 app.use(express.json());
