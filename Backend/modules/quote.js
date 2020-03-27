737789f0e4e79c29dc355350cf52ed5c3bae3898
Felix Schütz <mail@felix-schuetz.de>
2020-03-27 16:34:23 +0100

    Add tests for the quote module

--- Backend/modules/quote.js
+++ Backend/modules/quote.js
@@ -1,20 +1,18 @@
-const pino = require('pino');
 const axios = require('axios').default;
-
-const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
 
 const endpoint = 'https://quotes.rest/qod';
 
 // TODO from preferences
-const quoteCategory = 'funny';
+const category = 'funny';
 
+// TODO cache?
 async function getPreferredQuoteOfTheDay() {
   try {
-    const response = await axios.get(endpoint, { params: { category: quoteCategory } });
+    const response = await axios.get(endpoint, { params: { category } });
     const { quote, author } = response.data.contents.quotes[0];
     return { quote, author };
   } catch (error) {
-    if (error.response && error.response.status === 429) {
+    if (error.response.status === 429) {
       return { quote: 'Only a stupid man would request that many quotes.', author: 'Gunter' };
     }
 
@@ -22,4 +20,4 @@ async function getPreferredQuoteOfTheDay() {
   }
 }
 
-module.exports = { endpoint, getPreferredQuoteOfTheDay };
+module.exports = { endpoint, category, getPreferredQuoteOfTheDay };
