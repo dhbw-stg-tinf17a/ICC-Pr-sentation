3f4b43dcf6ae4cff9daff8bf6e7b9f2e4423ec40
Felix Schütz <mail@felix-schuetz.de>
2020-03-28 15:08:16 +0100

    Rename calendar methods

--- Backend/tests/calendar.test.js
+++ Backend/tests/calendar.test.js
@@ -6,11 +6,11 @@ jest.mock('node-ical');
 jest.mock('../modules/preferences');
 
 describe('calendar module', () => {
-  describe('getFirstEventOfDay', () => {
+  describe('getFirstEventStartingBetween', () => {
     it('should throw an error if no calendar URL is set', async () => {
       preferences.get.mockResolvedValue({});
 
-      await expect(calendar.getFirstEventOfDay(new Date('2020-03-28'))).rejects.toThrow('Calendar URL is not set');
+      await expect(calendar.getFirstEventStartingBetween({ start: '2020-03-28T00:00:00Z', end: '2020-03-29T00:00:00Z' })).rejects.toThrow('Calendar URL is not set');
     });
 
     it('should return the first event of today if it did not start yet', async () => {
@@ -23,31 +23,31 @@ describe('calendar module', () => {
       const inTwoMinutes = new Date(now.getTime());
       inTwoMinutes.setUTCMinutes(inTwoMinutes.getUTCMinutes() + 1);
       const events = {
-        1: { type: 'VEVENT', start: new Date('2020-03-28T08:00:00') },
-        2: { type: 'VEVENT', start: new Date('2020-03-28T07:00:00') },
+        1: { type: 'VEVENT', start: new Date('2020-03-28T08:00:00Z') },
+        2: { type: 'VEVENT', start: new Date('2020-03-28T07:00:00Z') },
       };
       ical.async.fromURL.mockResolvedValue(events);
 
-      await expect(calendar.getFirstEventOfDay(new Date('2020-03-28'))).resolves.toStrictEqual(events[2]);
+      await expect(calendar.getFirstEventStartingBetween({ start: '2020-03-28T00:00:00Z', end: '2020-03-29T00:00:00Z' })).resolves.toStrictEqual(events[2]);
 
       // check conversion to API request (only in this test case)
       expect(ical.async.fromURL).toHaveBeenLastCalledWith(calendarURL);
     });
   });
 
-  describe('getFreeSlots', () => {
+  describe('getFreeSlotsBetween', () => {
     it('should return all free slots during the given time', async () => {
       const calendarURL = 'https://example.com';
       preferences.get.mockResolvedValue({ calendarURL });
 
       ical.async.fromURL.mockResolvedValue({
-        1: { type: 'VEVENT', start: new Date('2020-03-27T00:00'), end: new Date('2020-03-27T07:00') },
-        2: { type: 'VEVENT', start: new Date('2020-03-27T12:00'), end: new Date('2020-03-27T15:00') },
-        3: { type: 'VEVENT', start: new Date('2020-03-28T07:00'), end: new Date('2020-03-28T09:00') },
+        1: { type: 'VEVENT', start: new Date('2020-03-27T00:00Z'), end: new Date('2020-03-27T07:00Z') },
+        2: { type: 'VEVENT', start: new Date('2020-03-27T12:00Z'), end: new Date('2020-03-27T15:00Z') },
+        3: { type: 'VEVENT', start: new Date('2020-03-28T07:00Z'), end: new Date('2020-03-28T09:00Z') },
       });
 
-      await expect(calendar.getFreeSlots({ start: '2020-03-27T00:00', end: '2020-03-27T15:00' })).resolves.toStrictEqual([
-        { start: new Date('2020-03-27T07:00'), end: new Date('2020-03-27T12:00') },
+      await expect(calendar.getFreeSlotsBetween({ start: '2020-03-27T00:00Z', end: '2020-03-27T15:00Z' })).resolves.toStrictEqual([
+        { start: new Date('2020-03-27T07:00Z'), end: new Date('2020-03-27T12:00Z') },
       ]);
     });
   });
