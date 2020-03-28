ee39009fb468fd5e8136188f84000181b5703a31
Felix Schütz <mail@felix-schuetz.de>
2020-03-28 12:41:15 +0100

    Use morgan for request logging

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,13 +1,6 @@
 const express = require('express');
-const pino = require('pino');
 
 const router = express.Router();
-const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
-
-router.use('/', (req, res, next) => {
-  logger.info(`${req.method} ${req.originalUrl}`);
-  next();
-});
 
 router.use('/preferences', require('./preferences'));
 router.use('/notifications', require('./notifications'));
--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 
 const express = require('express');
 const helmet = require('helmet');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 
 app.use('/api', require('./api'));
 
