0d41915ae5ac7ecedb75509fc2ae0de8e1f39982
Felix Schütz <mail@felix-schuetz.de>
2020-04-03 16:07:33 +0200

    Setup Vue 3 frontend test

---
+++ Frontend_Test/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Hello Vue 3!</h1>
+  <button @click="inc">I was clicked {{ count }} times</button>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const count = ref(0);
+    const inc = () => {
+      count.value++;
+    };
+
+    return {
+      count,
+      inc
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ Frontend_Test/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = (env = {}) => ({
+  mode: env.prod ? 'production' : 'development',
+  devtool: env.prod ? 'source-map' : 'cheap-module-eval-source-map',
+  entry: path.resolve(__dirname, './src/main.js'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/'
+  },
+  resolve: {
+    alias: {
+      'vue': '@vue/runtime-dom'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.png$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 8192 }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { hmr: !env.prod }
+          },
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
+  devServer: {
+    hot: true,
+    stats: 'minimal',
+    contentBase: path.join(__dirname, 'public'),
+    overlay: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+      },
+    },
+  }
+})
