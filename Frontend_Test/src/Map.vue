c87c354c2ca612880bfaf1cc1d3bcbc3a3330e4c
Felix Schütz <mail@felix-schuetz.de>
2020-04-04 13:32:38 +0200

    Draw line on map

--- Frontend_Test/src/Map.vue
+++ Frontend_Test/src/Map.vue
@@ -6,14 +6,56 @@
 import mapboxgl from "mapbox-gl";
 import { onMounted } from "vue";
 
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZnNjaHVldHowNCIsImEiOiJjam12cnNxd2swOHI0M3ZvMThmbG1qMjdlIn0.AQOATykYs-7IwFRYxoNVGQ";
+
 export default {
   setup() {
     onMounted(() => {
-      mapboxgl.accessToken =
-        "pk.eyJ1IjoiZnNjaHVldHowNCIsImEiOiJjam12cnNxd2swOHI0M3ZvMThmbG1qMjdlIn0.AQOATykYs-7IwFRYxoNVGQ";
       const map = new mapboxgl.Map({
         container: "map",
-        style: "mapbox://styles/mapbox/streets-v9"
+        style: "mapbox://styles/mapbox/streets-v9",
+        bounds: [
+          [9.17702, 48.78232],
+          [8.68417, 50.11552]
+        ],
+        fitBoundsOptions: {
+          padding: 100
+        }
+      });
+
+      map.on("load", () => {
+        map.addSource("lines", {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: [
+              {
+                type: "Feature",
+                properties: {
+                  color: "#F7455D" // red
+                },
+                geometry: {
+                  type: "LineString",
+                  coordinates: [
+                    [9.17702, 48.78232],
+                    [8.68417, 50.11552]
+                  ]
+                }
+              }
+            ]
+          }
+        });
+
+        map.addLayer({
+          id: "lines",
+          type: "line",
+          source: "lines",
+          paint: {
+            "line-width": 3,
+            "line-color": ["get", "color"]
+          }
+        });
       });
     });
   }
