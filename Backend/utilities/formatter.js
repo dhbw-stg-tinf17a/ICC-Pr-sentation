d20bf993078269c41bfbf41f64a8849537f063ad
Felix Schütz <mail@felix-schuetz.de>
2020-04-24 10:34:21 +0200

    Format datetime relative to current time

--- Backend/utilities/formatter.js
+++ Backend/utilities/formatter.js
@@ -4,7 +4,19 @@ const moment = require('moment-timezone');
 const timezone = 'Europe/Berlin';
 
 function formatDatetime(datetime) {
-  return moment.tz(datetime, timezone).format('dddd hh:mm A');
+  const localDatetime = moment.tz(datetime, timezone);
+  const today = moment.tz(timezone);
+  const tomorrow = today.clone().add(1, 'day');
+
+  if (localDatetime.isSame(today, 'day')) {
+    return `Today ${localDatetime.format('hh:mm A')}`;
+  }
+
+  if (localDatetime.isSame(tomorrow, 'day')) {
+    return `Tomorrow ${localDatetime.format('hh:mm A')}`;
+  }
+
+  return localDatetime.format('dddd hh:mm A');
 }
 
 function formatTime(datetime) {
