c35b65f56afea80eb0765e02839029868548dd0a
Felix Schütz <mail@felix-schuetz.de>
2020-04-24 16:47:29 +0200

    Add tests for preferences API

--- Backend/api/preferences.test.js
+++ Backend/api/preferences.test.js
@@ -2,19 +2,35 @@ jest.mock('morgan', () => () => (req, res, next) => next());
 
 const supertest = require('supertest');
 const app = require('../app');
+const preferences = require('../modules/preferences');
+
+jest.mock('../modules/preferences');
 
 const request = supertest(app);
 
 describe('/api/preferences', () => {
   describe('GET /', () => {
     it('should work', async () => {
-      // const response = request.get('/api/preferences');
+      const pref = { calendarURL: 'https://example.com/a' };
+      preferences.get.mockResolvedValueOnce(pref);
+
+      const response = await request.get('/api/preferences');
+
+      expect(response.status).toStrictEqual(200);
+      expect(response.body).toStrictEqual(pref);
     });
   });
 
   describe('PATCH /', () => {
     it('should work', async () => {
-      // const response = request.patch('/api/preferences');
+      preferences.update.mockResolvedValueOnce();
+
+      const pref = { calendarURL: 'https://example.com/a' };
+      const response = await request.patch('/api/preferences').send(pref);
+
+      expect(response.status).toStrictEqual(200);
+      expect(preferences.update).toHaveBeenCalledTimes(1);
+      expect(preferences.update).toHaveBeenLastCalledWith(pref);
     });
   });
 });
