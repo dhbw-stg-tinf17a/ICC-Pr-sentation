9e5bf6a454e16182f3460e3affdec7a02169f082
Felix Schütz <mail@felix-schuetz.de>
2020-04-24 11:55:19 +0200

    Add test for /api/notifications/enable

--- Backend/api/notifications.test.js
+++ Backend/api/notifications.test.js
@@ -2,13 +2,41 @@ jest.mock('morgan', () => () => (req, res, next) => next());
 
 const supertest = require('supertest');
 const app = require('../app');
+const notifications = require('../modules/notifications');
+
+jest.mock('../modules/notifications');
 
 const request = supertest(app);
 
 describe('/api/notifications', () => {
   describe('POST /enable', () => {
-    it('should work', async () => {
-      // const response = request.post('/api/notifications/enable');
+    it('should store the subscription and send a notification', async () => {
+      notifications.addSubscription.mockResolvedValueOnce();
+      notifications.sendNotification.mockResolvedValueOnce();
+
+      const subscription = {
+        endpoint: 'https://example.com/a',
+        expirationTime: null,
+        keys: { p256dh: 'p256dh_key_a', auth: 'auth_key_a' },
+      };
+
+      const response = await request.post('/api/notifications/enable').send(subscription);
+
+      expect(response.status).toStrictEqual(200);
+      expect(notifications.addSubscription).toHaveBeenCalledTimes(1);
+      expect(notifications.addSubscription).toHaveBeenLastCalledWith(subscription);
+      expect(notifications.sendNotification).toHaveBeenCalledTimes(1);
+      expect(notifications.sendNotification).toHaveBeenLastCalledWith(
+        {
+          title: 'A notification from Gunter!',
+          options: {
+            body: 'It works :)',
+            icon: '/favicon.jpg',
+            badge: '/badge.png',
+          },
+        },
+        subscription,
+      );
     });
   });
 
