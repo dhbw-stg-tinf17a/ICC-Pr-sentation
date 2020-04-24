b5c2f0dbafe86a9c5b13e2a1f9d124d7677c0fe2
Felix Schütz <mail@felix-schuetz.de>
2020-04-24 16:39:37 +0200

    Complete tests for the travel planning API

--- Backend/api/travel-planning.js
+++ Backend/api/travel-planning.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const travelPlanning = require('../usecases/travel-planning');
 const wrapAsync = require('../utilities/wrap-async');
-const { formatDatetime, formatTime } = require('../utilities/formatter');
-const db = require('../modules/db');
+const { formatDatetime, formatTime, formatConnection } = require('../utilities/formatter');
 
 const router = express.Router();
 
@@ -22,10 +21,19 @@ router.get('/', wrapAsync(async (req, res) => {
     arrival: sunday,
   });
 
+  const totalPrice = connectionToDestination.price + connectionFromDestination.price;
+
+  const {
+    saturdayWeatherForecast,
+    sundayWeatherForecast,
+  } = await travelPlanning.getWeatherForecast({
+    saturday,
+    sunday,
+    destination,
+  });
+
   let textToDisplay = '';
   let textToRead = '';
-  let furtherAction;
-  let nextLink;
 
   if (weekendFree) {
     textToDisplay += 'Free next weekend.\n';
@@ -33,47 +41,31 @@ router.get('/', wrapAsync(async (req, res) => {
   } else {
     textToDisplay += 'Not free next weekend.\n';
     textToRead += 'Unfortunately you are not free next weekend, but I will try to find a '
-      + 'travel destination anyway';
+      + 'travel destination anyway.\n';
   }
 
-  if (destination) {
-    const {
-      saturdayWeatherForecast,
-      sundayWeatherForecast,
-    } = await travelPlanning.getWeatherForecast({
-      saturday,
-      sunday,
-      destination,
-    });
+  textToDisplay += `Destination: ${destination.name}.\n`
+    + `To destination: ${formatDatetime(connectionToDestination.departure)} - `
+    + `${formatTime(connectionToDestination.arrival)}.\n`
+    + `From destination: ${formatDatetime(connectionFromDestination.departure)} - `
+    + `${formatTime(connectionFromDestination.arrival)}.\n`
+    + `Total price: ${totalPrice} €.\n`
+    + `Saturday weather: ${saturdayWeatherForecast.day.shortPhrase}.\n`
+    + `Sunday weather: ${sundayWeatherForecast.day.shortPhrase}.`;
 
-    const totalPrice = connectionToDestination.price + connectionFromDestination.price;
-
-    textToDisplay += `Destination: ${destination.name}.\n`
-      + `To destination: ${formatDatetime(connectionToDestination.departure)} - `
-      + `${formatTime(connectionToDestination.arrival)}.\n`
-      + `From destination: ${formatDatetime(connectionFromDestination.departure)} - `
-      + `${formatTime(connectionFromDestination.arrival)}.\n`
-      + `Total price: ${totalPrice} €.\n`
-      + `Saturday weather: ${saturdayWeatherForecast.day.shortPhrase}.\n`
-      + `Sunday weather: ${sundayWeatherForecast.day.shortPhrase}.`;
-
-    textToRead += `You could travel to ${destination.name}.\n`
-      + `You start from the main station ${formatDatetime(connectionToDestination.departure)} `
-      + `and arrive at ${formatTime(connectionToDestination.arrival)}.\n`
-      + `The journey back starts ${formatDatetime(connectionFromDestination.departure)} `
-      + `and ends at ${formatTime(connectionFromDestination.arrival)}.\n`
-      + `The total price will be ${totalPrice} Euros.\n`
-      + `The weather on Saturday will be ${saturdayWeatherForecast.day.longPhrase}.\n`
-      + `On Sunday it will be ${sundayWeatherForecast.day.longPhrase}.`;
+  textToRead += `You could travel to ${destination.name}.\n`
+    + `You start from the main station ${formatDatetime(connectionToDestination.departure)} `
+    + `and arrive at ${formatTime(connectionToDestination.arrival)}.\n`
+    + `The journey back starts ${formatDatetime(connectionFromDestination.departure)} `
+    + `and ends at ${formatTime(connectionFromDestination.arrival)}.\n`
+    + `The total price will be ${totalPrice} Euros.\n`
+    + `The weather on Saturday will be ${saturdayWeatherForecast.day.longPhrase}.\n`
+    + `On Sunday it will be ${sundayWeatherForecast.day.longPhrase}.`;
 
 
-    furtherAction = 'Do you want to know how to get to the main station?';
-    nextLink = 'travel-planning/confirm'
-      + `?arrival=${connectionToDestination.departure.toISOString()}`;
-  } else {
-    textToDisplay += 'No travel destination found.';
-    textToRead += 'Unfortunately I did not find a travel destination for the weekend. Sorry.';
-  }
+  const furtherAction = 'Do you want to know how to get to the main station?';
+  const nextLink = 'travel-planning/confirm'
+    + `?arrival=${connectionToDestination.departure.toISOString()}`;
 
   res.send({
     textToDisplay,
@@ -86,33 +78,22 @@ router.get('/', wrapAsync(async (req, res) => {
 router.get('/confirm', wrapAsync(async (req, res) => {
   const connection = await travelPlanning.getConnectionToMainStation(req.query.arrival);
 
-  let textToRead;
-  let textToDisplay;
-  let displayRouteOnMap;
+  let textToRead = '';
+  let textToDisplay = '';
+
   if (connection) {
-    textToDisplay = `Leave home: ${formatDatetime(connection.departure)}.\n`
-                    + `First stop: ${connection.legs[0].to}.\n`
-                    + `Destination: ${connection.legs[connection.legs.length - 1].to}`;
-    textToRead = `You have to leave at ${formatDatetime(connection.departure)}. `
-                  + `Your first stop will be ${connection.legs[0].to}. `
-                  + `Your destination is ${connection.legs[connection.legs.length - 1].to}`;
-    displayRouteOnMap = {
-      origin: connection.legs[0].from,
-      destination: connection.legs[connection.legs.length - 1].to,
-    };
+    textToDisplay += `Leave at: ${formatTime(connection.departure)}.\n`
+      + `Go to ${formatConnection(connection)}.`;
+    textToRead += `You have to leave at ${formatTime(connection.departure)}.\n`
+                  + `Go to ${formatConnection(connection)}.`;
   } else {
-    textToDisplay = 'did not find route to starting point of your travel! Sorry!';
-    textToRead = 'I did not find a route to your travel starting point. Sorry!';
-    displayRouteOnMap = null;
+    textToDisplay += 'No route found.';
+    textToRead += 'I did not find a route to the main station. Sorry!';
   }
 
   res.send({
     textToDisplay,
     textToRead,
-    displayRouteOnMap,
-    displayPointOnMap: null,
-    furtherAction: null,
-    nextLink: null,
   });
 }));
 
--- Backend/api/travel-planning.test.js
+++ Backend/api/travel-planning.test.js
@@ -80,14 +80,97 @@ describe('/api/travel-planning', () => {
         nextLink: 'travel-planning/confirm?arrival=2020-01-18T08:00:00.000Z',
       });
     });
+
+    it('should generate a valid message with a busy weekend and a destination', async () => {
+      travelPlanning.getWeekend.mockResolvedValueOnce({
+        saturday: new Date('2020-01-18T12:00:00Z'),
+        sunday: new Date('2020-01-19T12:00:00Z'),
+        weekendFree: false,
+      });
+
+      travelPlanning.planRandomTrip.mockResolvedValueOnce({
+        destination: {
+          name: 'Frankurt (Main) Hbf',
+        },
+        connectionToDestination: {
+          departure: new Date('2020-01-18T08:00:00Z'),
+          arrival: new Date('2020-01-18T10:00:00Z'),
+          price: 20.9,
+        },
+        connectionFromDestination: {
+          departure: new Date('2020-01-19T18:00:00Z'),
+          arrival: new Date('2020-01-19T20:00:00Z'),
+          price: 31.6,
+        },
+      });
+
+      travelPlanning.getWeatherForecast.mockResolvedValueOnce({
+        saturdayWeatherForecast: {
+          day: {
+            shortPhrase: 'Breezy in the morning; sunny',
+            longPhrase: 'Breezy this morning; otherwise, chilly with plenty of sunshine',
+          },
+        },
+        sundayWeatherForecast: {
+          day: {
+            shortPhrase: 'Breezy all day',
+            longPhrase: 'Breezy all day with some clouds',
+          },
+        },
+      });
+
+      const response = await request.get('/api/travel-planning');
+
+      expect(response.status).toStrictEqual(200);
+      expect(response.body).toStrictEqual({
+        textToDisplay: 'Not free next weekend.\nDestination: Frankurt (Main) Hbf.\n'
+          + 'To destination: Saturday 09:00 AM - 11:00 AM.\n'
+          + 'From destination: Sunday 07:00 PM - 09:00 PM.\nTotal price: 52.5 €.\n'
+          + 'Saturday weather: Breezy in the morning; sunny.\nSunday weather: Breezy all day.',
+        textToRead: 'Unfortunately you are not free next weekend, but I will try to find a '
+          + 'travel destination anyway.\nYou could travel to Frankurt (Main) Hbf.\n'
+          + 'You start from the main station Saturday 09:00 AM and arrive at 11:00 AM.\n'
+          + 'The journey back starts Sunday 07:00 PM and ends at 09:00 PM.\n'
+          + 'The total price will be 52.5 Euros.\nThe weather on Saturday will be Breezy this '
+          + 'morning; otherwise, chilly with plenty of sunshine.\nOn Sunday it will be Breezy all '
+          + 'day with some clouds.',
+        furtherAction: 'Do you want to know how to get to the main station?',
+        nextLink: 'travel-planning/confirm?arrival=2020-01-18T08:00:00.000Z',
+      });
+    });
   });
 
   describe('GET /confirm', () => {
-    it('should work', async () => {
-      /* const response = await request.get('/api/travel-planning/confirm');
+    it('should generate a valid message with a connection', async () => {
+      travelPlanning.getConnectionToMainStation.mockResolvedValueOnce({
+        departure: '2020-01-15T16:10:00Z',
+        legs: [
+          { to: 'Talstraße' },
+          { to: 'Bergstraße' },
+        ],
+      });
+
+      const response = await request.get('/api/travel-planning/confirm'
+        + '?arrival=2020-01-18T08:00:00.000Z');
 
       expect(response.status).toStrictEqual(200);
-      expect(response.body).toStrictEqual({}); */
+      expect(response.body).toStrictEqual({
+        textToDisplay: 'Leave at: 05:10 PM.\nGo to Talstraße, then Bergstraße.',
+        textToRead: 'You have to leave at 05:10 PM.\nGo to Talstraße, then Bergstraße.',
+      });
+    });
+
+    it('should generate a valid message with no connection', async () => {
+      travelPlanning.getConnectionToMainStation.mockResolvedValueOnce();
+
+      const response = await request.get('/api/travel-planning/confirm'
+        + '?arrival=2020-01-18T08:00:00.000Z');
+
+      expect(response.status).toStrictEqual(200);
+      expect(response.body).toStrictEqual({
+        textToDisplay: 'No route found.',
+        textToRead: 'I did not find a route to the main station. Sorry!',
+      });
     });
   });
 });
